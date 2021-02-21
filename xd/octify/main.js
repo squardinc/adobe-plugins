9f07b0a127394cdc60870bc19644570f924552c0
Shoya <touka.nt.gh@gmail.com>
2021-02-21 17:51:39 +0900

    format

--- xd/octify/main.js
+++ xd/octify/main.js
@@ -1,18 +1,17 @@
 const octify = (number) => Math.round(number / 8) * 8;
 
-
 const octifyFn = (selection) => {
-  const { items } = selection
-  if(items.length === 0) return
-  if(items.length === 1) {
-    const item = items[0]
+  const { items } = selection;
+  if (items.length === 0) return;
+  if (items.length === 1) {
+    const item = items[0];
     const bounds = item.boundsInParent;
     item.resize(octify(bounds.width), octify(bounds.height));
-    return
+    return;
   }
 
   items.forEach((item) => {
-    console.log(item)
+    console.log(item);
   });
 };
 
@@ -29,31 +28,41 @@ const octifyHeightFn = (selection) => {
   });
 };
 const octifyDifferenceFn = (selection) => {
-  const { items } = selection
-  if(items.length === 2) {
-    const itemOne = items[0]
-    const itemTwo = items[1]
-    const itemOneRightBottom = {x: itemOne.topLeftInParent.x + itemOne.width, y: itemOne.topLeftInParent.y + itemOne.height};
-  
-    if(itemOneRightBottom.y > itemTwo.topLeftInParent.y && itemOneRightBottom.x < itemTwo.topLeftInParent.x) {
-      const diffX = itemTwo.topLeftInParent.x - (itemOne.topLeftInParent.x + itemOne.width);
+  const { items } = selection;
+  if (items.length === 2) {
+    const itemOne = items[0];
+    const itemTwo = items[1];
+    const itemOneRightBottom = {
+      x: itemOne.topLeftInParent.x + itemOne.width,
+      y: itemOne.topLeftInParent.y + itemOne.height,
+    };
+
+    if (
+      itemOneRightBottom.y > itemTwo.topLeftInParent.y &&
+      itemOneRightBottom.x < itemTwo.topLeftInParent.x
+    ) {
+      const diffX =
+        itemTwo.topLeftInParent.x - (itemOne.topLeftInParent.x + itemOne.width);
       itemTwo.moveInParentCoordinates(octify(diffX) - diffX, 0);
-    } 
-    else if((itemOneRightBottom.y < itemTwo.topLeftInParent.y) && (itemOneRightBottom.x > itemTwo.topLeftInParent.x))
-    {
-      const diffY = itemTwo.topLeftInParent.y - (itemOne.topLeftInParent.y + itemOne.height);
-      itemTwo.moveInParentCoordinates( 0, octify(diffY) - diffY);  
+    } else if (
+      itemOneRightBottom.y < itemTwo.topLeftInParent.y &&
+      itemOneRightBottom.x > itemTwo.topLeftInParent.x
+    ) {
+      const diffY =
+        itemTwo.topLeftInParent.y -
+        (itemOne.topLeftInParent.y + itemOne.height);
+      itemTwo.moveInParentCoordinates(0, octify(diffY) - diffY);
     } else {
-    return
-    }}
-}
+      return;
+    }
+  }
+};
 
 module.exports = {
   commands: {
     octify: octifyFn,
     octifyWidth: octifyWidthFn,
     octifyHeight: octifyHeightFn,
-    octifyDiff: octifyDifferenceFn
-
+    octifyDiff: octifyDifferenceFn,
   },
 };
