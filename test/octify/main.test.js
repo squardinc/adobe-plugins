e95d6f0a536426f3818ea100c7bee737a6cc233d
Yuuki <k.yuuki010708@gmail.com>
2021-02-23 17:42:18 +0900

    octifyDifferenceFnの横並び判定のtestを追加。

--- test/octify/main.test.js
+++ test/octify/main.test.js
@@ -1,5 +1,5 @@
 const { describe, test, expect } = require("@jest/globals");
-const { octify } = require("../../xd/octify/main");
+const { octify ,shouldPlaceToHorizontal} = require("../../xd/octify/main");
 
 describe("Octify", () => {
   describe("Octify", () => {
@@ -14,6 +14,62 @@ describe("Octify", () => {
     test("no change if divisible", () => {
       expect(octify(8)).toBe(8);
       expect(octify(16)).toBe(16);
-    });
+    }); 
   });
+
+  describe("shouldPlaceToHorizontal", () => {
+    const firstItem = {
+      topLeftInParent: {
+        x: 0,
+        y: 0,
+      },
+      width: 10,
+      height:20,
+    }
+
+    const items = [
+      firstItem,
+      {
+        topLeftInParent: {
+          x: firstItem.width + 1,
+          y: firstItem.height-1,
+        },
+      },
+      {
+        topLeftInParent: {
+          x: firstItem.width + 0.1,
+          y: firstItem.height-0.1,
+        },
+      }
+    ]
+    test("true if no items placed in bottom of first item", () => {
+      expect(shouldPlaceToHorizontal(items)).toBeTruthy();
+    })
+    test("false if on or more items placed in bottom of first item", () => {
+      const items2 = [
+        items[0],
+        {
+          topLeftInParent: {
+            x: items[0].topLeftInParent.x, 
+            y: firstItem.height+1,
+          }
+        },
+        items[2],
+      ]
+      expect(shouldPlaceToHorizontal(items2)).toBeFalsy();
+    })
+    test("false if on or more items placed in left of first item", () => {
+      const items3 = [
+        items[0],
+        {
+          topLeftInParent: {
+            x: firstItem.width-1, 
+            y: items[1].height,
+          }
+        },
+        items[2],
+      ]
+      expect(shouldPlaceToHorizontal(items3)).toBeFalsy();
+    })
+  })
 });
