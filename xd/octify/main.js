7cb5bf59352f29db4d816f2e8a29d427049b843d
Shoya <touka.nt.gh@gmail.com>
2021-02-23 21:37:57 +0900

    bug fix for rotated objects

--- xd/octify/main.js
+++ xd/octify/main.js
@@ -2,20 +2,20 @@ const octify = (number) => Math.round(number / 8) * 8;
 
 const octifyFn = (selection) => {
   selection.items.forEach((item) => {
-    const bounds = item.boundsInParent;
+    const bounds = item.localBounds;
     item.resize(octify(bounds.width), octify(bounds.height));
   });
 };
 
 const octifyWidthFn = (selection) => {
   selection.items.forEach((item) => {
-    const bounds = item.boundsInParent;
+    const bounds = item.localBounds;
     item.resize(octify(bounds.width), bounds.height);
   });
 };
 const octifyHeightFn = (selection) => {
   selection.items.forEach((item) => {
-    const bounds = item.boundsInParent;
+    const bounds = item.localBounds;
     item.resize(bounds.width, octify(bounds.height));
   });
 };
